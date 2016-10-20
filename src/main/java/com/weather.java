1e8799ec3337f21a1d1e04654103621de9a49783
Mtdr <paul.osmolovsky@gmail.com>
2016-10-21 01:19:47 +0300

    0.0.1b(1)

--- src/main/java/com/weather.java
+++ src/main/java/com/weather.java
@@ -16,8 +16,8 @@ public class weather {
 
     // 2122265 - moscow
     // 2123260 - saint-p
-        public static void main(String[] args) throws JAXBException, IOException {
-            YahooWeatherService service = new YahooWeatherService();
+    public static void main(String[] args) throws JAXBException, IOException {
+        YahooWeatherService service = new YahooWeatherService();
 //            Channel result = service.getForecast("2123260", DegreeUnit.CELSIUS);
 //            System.out.println(result.getDescription());
 //            System.out.println(result.getTitle());
@@ -26,26 +26,23 @@ public class weather {
 //            System.out.println(result.getLink());
 //            System.out.println(result.getItem().getForecasts());
 //            System.out.println(result.getItem().getCondition());
+        String id = "Moscow";
 
-
-            List<Channel> channels = service.getForecastForLocation("Moscow", DegreeUnit.CELSIUS).first(3);
-            for (Channel channel:channels){
-                System.out.println(channel.getTitle());
-                System.out.println(channel.getItem().getCondition());
-            }
+        List<Channel> channels = service.getForecastForLocation(id, DegreeUnit.CELSIUS).first(1);
+        for (Channel channel:channels){
+            System.out.println(channel.getTitle());
+            System.out.println(channel.getItem().getCondition());
         }
+    }
 
     public static String showWeather(String idCity) throws JAXBException, IOException {
+        String res;
+        res = "";
         YahooWeatherService service = new YahooWeatherService();
-        Channel result = service.getForecast(idCity, DegreeUnit.CELSIUS);
-        System.out.println(result.getDescription());
-        System.out.println(result.getTitle());
-        System.out.println(result.getWind());
-        System.out.println(result.getAtmosphere());
-        System.out.println(result.getLink());
-        System.out.println(result.getItem().getForecasts());
-        System.out.println(result.getItem().getCondition());
-        return result.getItem().getCondition().toString();
+        List<Channel> channels = service.getForecastForLocation(idCity, DegreeUnit.CELSIUS).first(1);
+        for (Channel channel:channels)
+            res = channel.getItem().getCondition().toString();
+        return res;
     }
 
-    }
+}
